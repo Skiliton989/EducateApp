e857d29546da35413941c6788792c1a89f696630
Алексей Кузьмин <skiliton989@gmail.com>
2021-10-27 08:47:59 +0500

    Создание контекста данных, написание строки подключения к любому экземпляру MS SQL Server, связи контекста и строки в файле Startup

---
+++ EducateAPP/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducateAPP.Models
+{
+    public class User : IdentityUser
+    {
+        //дополнительные поля для каждого пользователя
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+    }
+}
--- EducateAPP/Startup.cs
+++ EducateAPP/Startup.cs
@@ -1,6 +1,9 @@
+using EducateAPP.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,12 @@ namespace EducateAPP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppCtx>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppCtx>();
+
             services.AddControllersWithViews();
         }
 
@@ -44,6 +53,7 @@ namespace EducateAPP
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
